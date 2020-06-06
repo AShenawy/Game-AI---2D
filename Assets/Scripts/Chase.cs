a0a9bb8118a6e2d3fd4f6cc3d735b7f828f1be0e
Alper Üste <alperuste@gmail.com>
2020-06-06 19:43:16 +0300

    Initialize first complete project

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    private Patrol PatrolScript;
+
+    public float speed;
+
+    [SerializeField]
+    bool isChasing = false;
+
+    Transform player;
+
+
+    public GameObject GameOverPanel;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PatrolScript = this.GetComponent<Patrol>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing)
+        {
+            ChaseThePlayer();
+        }
+        else if (!isChasing)
+        {
+
+        }
+    }
+
+    void ChaseThePlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag("Player"))
+        {
+            player = target.transform;
+            isChasing = true;
+            PatrolScript.enabled = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D target)
+    {
+        if (target.CompareTag("Player"))
+        {
+            player = target.transform;
+        }
+    }
+
+    
+
+    private void OnTriggerExit2D(Collider2D target)
+    {
+        if (target.CompareTag("Player"))
+        {
+            isChasing = false;
+            PatrolScript.enabled = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Time.timeScale = 0.00f;
+        GameOverPanel.active = true;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 10F;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = new Vector2(this.transform.position.x,this.transform.position.y + (MoveSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - (MoveSpeed * Time.deltaTime));
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = new Vector2(this.transform.position.x - (MoveSpeed * Time.deltaTime), this.transform.position.y);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = new Vector2(this.transform.position.x + (MoveSpeed * Time.deltaTime), this.transform.position.y);
+
+        }
+    }
+
+}
